1633e7bf5ad1622c67674a20a76d8d31d60e936b
SNikitinKRD <114020862+SNikitinKRD@users.noreply.github.com>
2022-10-27 00:21:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 from extensions import APIException, Convertor
-from config import TOKEN, exchange
+from config import TOKEN, exchanges
 
 exchanges = {
     'доллар': 'USD',
@@ -11,32 +11,63 @@ TOKEN = "5683804532:AAEvqWDS6BF510qodmHntckchVLgoA5JqpQ"
 
 bot = telebot.TeleBot(TOKEN)
 
+
 @bot.message_handler(commands=['start', 'help'])
 def start(message: telebot.types.Message):
     text = "Приветствие!"
     bot.send_message(message.chat.id, text)
+
 
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
     for i in exchanges.keys():
         text = '\n'.join((text, i))
-    bot.reply_to(message, text)
+    bot.send_message(message.chat.id, text)
+
+@bot.message_handler(commands=['convert'])
+def values(message: telebot.types.Message):
+    text = 'Выберите валюту, из которой конвертировать:'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, base_handler)
+
+def base_handler(message: telebot.types.Message):
+    base = message.text.strip()
+    text = 'Выберите валюту, в которую конвертировать:'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, sym_handler, base)
+
+def sym_handler(message: telebot.types.Message, base):
+    sym = message.text.strip()
+    text = 'Выберите количество конвертируемой валюты:'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, amount_handler, base, sym)
+
+def amount_handler(message: telebot.types.Message, base, sym):
+    amount = message.text.strip()
+    try:
+        new_price = Convertor.get_price(base, sym, amount)
+    except APIException as e:
+        bot.send_message(message.chat.id, f"Ошибка конвертации: \n{e}")
+    else:
+        text = f"Цена {amount} {base} в {sym} : {new_price}"
+        bot.send_message(message.chat.id, text)
 
 @bot.message_handler(content_types=['text'])
 def converter(message: telebot.types.Message):
-    values = message.text.split(' ')
+    values = message.text.split('')
     try:
         if len(values) != 3:
             raise APIException('Неверное количество параметров!')
-        
+
         answer = Convertor.get_price(*values)
     except APIException as e:
-        bot.reply_to(message, f"Ошибка в команде:\n{e}" )
+        bot.reply_to(message, f"Ошибка в команде:\n{e}")
     except Exception as e:
         traceback.print_tb(e.__traceback__)
-        bot.reply_to(message, f"Неизвестная ошибка:\n{e}" )
+        bot.reply_to(message, f"Неизвестная ошибка:\n{e}")
     else:
-        bot.reply_to(message,answer)
+        bot.reply_to(message, answer)
+
 
 bot.polling()
